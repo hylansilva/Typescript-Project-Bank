af69939a38bccd89c70e6c64fd78fac3119da6fa
hylansilva <hylan.00000847839@unicap.br>
2023-03-31 19:19:16 -0300

    :construction: feat: HTTP Methods

--- api/src/controllers/accountControllers.ts
+++ api/src/controllers/accountControllers.ts
@@ -1,32 +1,92 @@
 import { Request, Response } from "express";
-import { prismaClient } from "../client/prismaClient";
+import { prisma } from "../client/prismaClient";
 
-export class CreateAccountController {
+
+export class AccountController{
+ // CREATE
 	async createAccount(request:Request, response:Response) {
 		const { name, balance, type} = request.body;
 
-		const account = await prismaClient.account.create({
+		const account = await prisma.account.create({
 			data:{
 				name,
 				balance,
 				type,
 			},
-		})
-		return response.status(200).json(account);
+		});
+		try {
+			return response.status(202).json({message:'Account has been created'});
+		} catch (error) {
+			return response.status(401).json({message:'Unauthorized'});
+		}
 	}
+	//READ
+	async findAllAccounts(request:Request, response:Response){
 
-}
-
-export class FindAllAccountController{
-	async findAccount(request:Request, response:Response){
-
-		const account = await prismaClient.account.findMany();
+		const account = await prisma.account.findMany();
 
 		return response.json(account);
 	}
 
-	async findExpecificAccount(request:Request, response:Response){
+	async findAccountByName(request:Request, response:Response) {
+		const  id = request.params.name;
+		const account = await prisma.account.findUnique({
+			where:{
+				name: id
+			}
+		})
+			return response.status(200).json({account: account})
+	}
+	//UPDATE
+	async updateAccountBalance(request:Request, response:Response){
+		const id = request.params.name;
+		const { balance } = request.body;
+		const findAccount = await prisma.account.findUnique({
+			where:{
+				name: id
+			},
+			select: {
+				balance
+			}
+		})
 
+		let amontOne = Number(balance);
+		let amontTwo = Number(findAccount);
+
+		let finalBalance = amontOne+ amontTwo
+
+		const updateAccount = await prisma.account.update({
+			data:{
+				balance: finalBalance
+			},
+			where:{
+				name:id
+			},
+		})
+		response.status(200).json({message:'Account has been updated'})
+	}
+	//DELETE
+	async deleteAllAccounts(request: Request, response: Response){
+		const accounts = await prisma.account.deleteMany({});
+		try {
+			return response.status(202).json({message:'All Accounts has been delected'});
+		} catch (error) {
+			return response.status(401).json({message:'Unauthorized'});
+		}
+	}
+
+	async deleteAccountByName(request:Request, response:Response){
+		const id = request.params.name;
+		const account = await prisma.account.delete({
+			where:{
+				name: id
+			},
+		});
+		try {
+			return response.status(202).json({message:'Account has been delected'});
+		} catch (error) {
+			return response.status(401).json({message:'Unauthorized'});
+		}
 	}
 }
 
--- api/src/routes/accountRoutes.ts
+++ api/src/routes/accountRoutes.ts
@@ -1,12 +1,25 @@
-import { Router } from "express";
-import { CreateAccountController, FindAllAccountController } from "../controllers/accountControllers";
+import { Router} from "express";
+import { AccountController } from "../controllers/accountControllers";
 
 export const router = Router();
 
 // Import the controller of Account Class
-const accountController = new CreateAccountController();
-const findController = new FindAllAccountController();
+const accountController = new AccountController();
+
 
 //creating the HTTP methods
+// GET Methods
+router.get('/account',accountController.findAllAccounts);
+router.get('/account/:name', accountController.findAccountByName);
+
+// POST Methods
 router.post('/account',accountController.createAccount);
-router.get('/account',findController.findAccount);
+
+// PUT Methods
+router.put('/account/:name', accountController.updateAccountBalance)
+
+// DELETE Methods
+router.delete('/account', accountController.deleteAllAccounts);
+router.delete('/account/:id', accountController.deleteAccountByName);
+
+
